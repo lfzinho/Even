b7f827deb43e36d8f76864cbc939a6b218f18fb7
Lf Laguardia <laguardialuis@gmail.com>
2022-02-21 14:47:23 -0300

    corrige timezones

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import agenda, submit
+import pytz
 from datetime import datetime
+
+tz_brasil = pytz.timezone("America/Sao_Paulo")
 
 st.image("Even-logo.png")
 st.write("Um quadro de notas comunitário & horários de aula rápido e prático.✅")
-st.caption(f"Última atualização: {datetime.now().strftime('%H:%M')}")
+st.caption(f"Última atualização: {datetime.now(tz_brasil).strftime('%H:%M')}")
 
 #selecionar página
 pag = st.sidebar.radio("Selecione sua Página:", ["Ver agenda", "Criar nova agenda"])
