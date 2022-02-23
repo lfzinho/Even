8e59be15f0cfd9b597edf95e703fc0f1d2a21f7b
Lf Laguardia <laguardialuis@gmail.com>
2022-02-23 09:42:29 -0300

    Add configs de página

--- main.py
+++ main.py
@@ -5,14 +5,25 @@ from datetime import datetime
 
 tz_brasil = pytz.timezone("America/Sao_Paulo")
 
+# configurações da página
+st.set_page_config(page_title= 'Even', 
+                   page_icon="⌚",
+                   initial_sidebar_state="collapsed",
+                   menu_items={
+                       "About": """Esse App foi desenvolvido por [@LfLaguardia](https://github.com/lfzinho).
+                        Sinta-se livre para requisitar funcionalidades ou só me dar um oi. 😃"""
+                   })
+
 st.image("Even-logo.png")
 st.write("Um quadro de notas comunitário & horários de aula rápido e prático.✅")
 st.caption(f"Última atualização: {datetime.now(tz_brasil).strftime('%H:%M')}")
 
-#selecionar página
+# selecionar página
 pag = st.sidebar.radio("Selecione sua Página:", ["Ver agenda", "Criar nova agenda"])
 
 if pag == "Ver agenda":
     agenda.render()
 elif pag == "Criar nova agenda":
     submit.render()
+
+
